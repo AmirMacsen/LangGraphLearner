0540b1a1def939c660c5b7d95fb9020cd5e0610f
AmirMacsen <3254168608@qq.com>
2025-08-30 13:32:49 +0800

    feature: langgraph output stream

---
+++ langgraph_stream/output_stream.py
@@ -0,0 +1,28 @@
+""""
+sub graph
+"""
+import operator
+from typing import Annotated
+
+from langgraph.constants import START, END
+from langgraph.graph import StateGraph
+from pydantic import BaseModel
+
+
+class MainState(BaseModel):
+    answer: Annotated[list, operator.add]
+
+def node1(state: MainState):
+    state.answer.append("answer")
+    return state
+
+
+main_builder = StateGraph(MainState)
+main_builder.add_node("node1", node1)
+main_builder.add_edge(START, "node1")
+main_builder.add_edge("node1", END)
+
+main_graph = main_builder.compile()
+
+for chunk in main_graph.stream(MainState(answer=["process node1"]), stream_mode="debug"):
+    print(chunk)
